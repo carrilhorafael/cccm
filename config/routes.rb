15924d7b65470471d0b33ce475dea0af955d4137
Rafael Carrilho <rafaelcarrilho@id.uff.br>
2021-11-21 13:52:29 -0300

    fix routes typo error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/validate_user', to "application#validate_user"
+  get '/validate_user', to: "application#validate_user"
   post "/auth/login", to: "auth#login"
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
