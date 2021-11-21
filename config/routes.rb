182d95314cf822748dede91dd0281cad501f6955
Rafael Carrilho <rafaelcarrilho@id.uff.br>
2021-11-21 13:27:44 -0300

    validate user get

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/validate_user', to "application#validate_user"
   post "/auth/login", to: "auth#login"
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
