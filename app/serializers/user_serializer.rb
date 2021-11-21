fd13270e2a75e0205d22dbeaf2e27a4520b7cc0c
Rafael Carrilho <rafaelcarrilho@id.uff.br>
2021-11-21 20:02:38 -0300

    add member_since on user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :rg, :cpf, :is_baptized, :last_time_logged_at, :age, :birthdate, :marital_status, :location, :branch, :is_master, :is_manager
+  attributes :id, :name, :email, :rg, :cpf, :is_baptized, :last_time_logged_at, :age, :birthdate, :member_since, :marital_status, :location, :branch, :is_master, :is_manager
 end
