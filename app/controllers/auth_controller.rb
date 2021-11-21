85c04441d5be1ef76c013b0e4d21563c4e50db50
Rafael Carrilho <rafaelcarrilho@id.uff.br>
2021-11-21 12:21:49 -0300

    fix de rota authentication

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,8 +1,7 @@
 class AuthController < ApplicationController
   def login
-    @user = User.find(email: params[:user][:email])
+    @user = User.find_by(email: params[:user][:email])
     @user = @user&.authenticate(params[:user][:password])
-    binding.pry
     if @user
       token = JsonWebToken::Base.encode(user_id: @user.id)
       @user.update!(last_time_logged_at: Time.zone.now)
