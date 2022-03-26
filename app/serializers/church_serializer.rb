5bc47e39c81ba2bf306598297a83db1dfbdfaac0
Rafael Carrilho <rafaelcarrilho@id.uff.br>
2022-03-26 11:01:41 -0300

    counts dos ministerios e membros

--- app/serializers/church_serializer.rb
+++ app/serializers/church_serializer.rb
@@ -1,3 +1,11 @@
 class ChurchSerializer < ActiveModel::Serializer
-  attributes :id, :name, :location, :is_head
+  attributes :id, :name, :location, :is_head, :users_count, :ministeries_count
+
+  def users_count
+    object.users.count
+  end
+
+  def ministeries_count
+    object.ministeries.count
+  end
 end
